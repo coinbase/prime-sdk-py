f343e663090ef1e6e81dddcfbd7ba8b1a57c7b1f
Carl Fluke <carl.fluke@coinbase.com>
2025-09-23 12:03:39 -0400

    version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prime-sdk-py",
-    version="1.1.0",
+    version="1.2.0",
     packages=find_packages(exclude=['prime_sdk.examples', 'prime_sdk.examples.*']),
     install_requires=[
         'requests',
